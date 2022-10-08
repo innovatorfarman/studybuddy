93a93dbfd2d360e334cc74dc6250eec9e0968964
Farman Khan <farmankhan0046@gmail.com>
2022-10-08 20:50:13 +0530

    Updated allowed hosts

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k48#^h1k)3stzk44t!l&nj#4o7qqcc(2*axw@*&o^c6^%2yw%*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition 
